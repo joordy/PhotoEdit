25215b0971a7d1b381d1d3f3d63c550b8d63d25c
joordy <jmfronik@gmail.com>
2021-02-02 16:52:35 +0100

    Working search query

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import 'regenerator-runtime/runtime'
 
 import { getMyData } from './utils/helpers/getMyData'
-// import { endpoint } from './utils/helpers/endpoints'
+import { endpoint } from './utils/helpers/endpoints'
 import { accessKey } from './utils/config/config'
 
 let clicked = false
@@ -10,35 +10,24 @@ button.addEventListener('click', functionName)
 
 function functionName(clicked) {
   clicked = true
-  // console.log(clicked);
+  console.log(clicked)
   checkDataSource(clicked)
 }
 
 const checkDataSource = (clicked) => {
   if (clicked == true) {
     console.log(clicked)
-    let url = 'https://api.unsplash.com/photos/?client_id='
+
+    let url = 'https://api.unsplash.com/search/photos/?client_id='
     let writtenQuery = document.getElementById('searchElement').value
     const abc = getMyData(`${url}${accessKey}&query=${writtenQuery}`)
-    const renderPagina = getResult(abc)
-
     console.log(`Searched with the Query: ${writtenQuery}`)
   } else if (clicked == false) {
     console.log(clicked)
     let url = 'https://api.unsplash.com/photos/?client_id='
     const cba = getMyData(`${url}${accessKey}`)
-    const renderPagina = getResult(cba)
     console.log(`Standard search`)
   }
 }
 
-const getResult = async (data) => {
-  data.forEach((elem) => {
-    let content = document.getElementById('dataWrapper')
-    let element = document.createElement(elem.urls.thumb)
-    element.src = image
-    content.appendChild(element)
-  })
-}
-
-// const ab = result();
+const ab = checkDataSource(clicked)
