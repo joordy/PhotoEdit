9a6e85ab30a9d99493f08541a4fb088eaf7b2306
joordy <jmfronik@gmail.com>
2021-02-02 16:06:27 +0100

    Added linter, changed data fetch

--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,44 @@
-import 'regenerator-runtime/runtime';
+import 'regenerator-runtime/runtime'
 
-import { getMyData } from './utils/helpers/getMyData';
-import { endpoint } from './utils/helpers/endpoints';
-import { accessKey } from './utils/config/config';
+import { getMyData } from './utils/helpers/getMyData'
+import { endpoint } from './utils/helpers/endpoints'
+import { accessKey } from './utils/config/config'
 
-let clicked = false;
-let button = document.querySelector('#searchPhotos');
-button.addEventListener('click', functionName);
+let clicked = false
+let button = document.querySelector('#searchPhotos')
+button.addEventListener('click', functionName)
 
 function functionName(clicked) {
-  clicked = true;
-  console.log(clicked);
-  checkDataSource(clicked);
+  clicked = true
+  // console.log(clicked);
+  checkDataSource(clicked)
 }
-
-console.log(clicked);
 
 const checkDataSource = (clicked) => {
   if (clicked == true) {
-    getMyData('https://opendata.rdw.nl/resource/b3us-f26s.json');
+    console.log(clicked)
+    let url = 'https://api.unsplash.com/photos/?client_id='
+    let writtenQuery = document.getElementById('searchElement').value
+    const abc = getMyData(`${url}${accessKey}&query=${writtenQuery}`)
+    const renderPagina = getResult(abc)
+
+    console.log(`Searched with the Query: ${writtenQuery}`)
   } else if (clicked == false) {
-    let url = 'https://api.unsplash.com/photos/';
-    let clientID = '?client_id=';
-    const cba = getMyData(`${url}${clientID}${accessKey}`);
+    console.log(clicked)
+    let url = 'https://api.unsplash.com/photos/?client_id='
+    const cba = getMyData(`${url}${accessKey}`)
+    const renderPagina = getResult(cba)
+    console.log(`Standard search`)
   }
-};
+}
 
-const ab = checkDataSource(clicked);
-// const checkDataSource = (accessKey) => {
-//   let clicked = false;
-//   let button = document.querySelector('#searchPhotos');
-//   button.addEventListener('click', functionName);
+const getResult = async (data) => {
+  data.forEach((elem) => {
+    let content = document.getElementById('dataWrapper')
+    let element = document.createElement(elem.urls.thumb)
+    element.src = image
+    content.appendChild(element)
+  })
+}
 
-//   if ((clicked = false)) {
-//     const abc = getMyData('https://opendata.rdw.nl/resource/b3us-f26s.json');
-//     return abc;
-//   } else {
-//     let url = 'https://api.unsplash.com/photos/';
-//     let clientID = '?client_id=';
-//     const cba = getMyData(`${url}${clientID}${accessKey}`);
-//     return ab;
-//   }
-// };
-
-// const ab = checkDataSource(accessKey);
-
-// console.log(ab);
-// getMyData(endpoint).then((abc) => {
-//   // console.log(endpoint);
-//   console.log(abc);
-// });
-
-// const button = document.querySelector('#searchPhotos');
-// button.addEventListener('click', () => {
-//   console.log('abcc');
-//   return abc
-// });
+// const ab = result();
