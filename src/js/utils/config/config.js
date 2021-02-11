3be5ad1a5b39fd359753c53193814973eac67cf7
joordy <jmfronik@gmail.com>
2021-02-11 23:30:16 +0100

    Added netlify redirects

---
+++ src/js/utils/config/config.js
@@ -0,0 +1,4 @@
+export const accessKey = '4ftBvwb6tBA2A3rXw88yOTkwD9RlZSQOyZHDAdstiuE'
+export const secretKey = 'ml5pbgS8CqpmoF4MJ98mooLTI5Xwv74nIE7EOPT-1Q4'
+export const globalUrl = 'https://api.unsplash.com/photos/?client_id='
+export const searchUrl = 'https://api.unsplash.com/search/photos/?client_id='
