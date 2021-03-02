904035752f05a4c29530effe81038ebc0df1eec5
joordy <jmfronik@gmail.com>
2021-03-02 13:01:05 +0100

    Fixing build error

--- src/js/utils/data/index.js
+++ src/js/utils/data/index.js
@@ -1,4 +1,4 @@
-import { checkID } from './_searchID'
+import { checkID } from './_checkID'
 import { fetchQuery } from './_fetchQuery'
 import { userInput } from './_events'
 import { LocalStorageSetup } from './_localStorage'
